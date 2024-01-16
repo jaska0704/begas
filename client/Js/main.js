88426e8eb53aac83f90a3a9ded5feb38dcde29b2
Jasurbek <jaskasamadov695@gmail.com>
2024-01-16 21:10:12 +0500

    tartar v

--- client/Js/main.js
+++ client/Js/main.js
@@ -10,4 +10,11 @@ btn2.addEventListener("click", (e) => {
   menu2.classList.toggle("dis_none");
 });
 
-
+let counter = 1;
+setInterval(function(){
+  document.querySelector("#radio" + counter).checked = true;
+  counter++;
+  if(counter>3){
+    counter=1;
+  }
+}, 3000)
